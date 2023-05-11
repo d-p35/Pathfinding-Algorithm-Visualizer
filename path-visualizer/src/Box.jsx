11b16b793d427db5127d67fd40c3af2f3583a60d
d-p35 <dhruv10203@gmail.com>
2023-05-11 01:25:01 -0400

    Added start and end points

--- path-visualizer/src/Box.jsx
+++ path-visualizer/src/Box.jsx
@@ -1,15 +1,30 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./Box.css";
 
-function Box() {
+function Box(props) {
 
-    const [isFinished, setIsFinished] = useState(false);
+    // const [isFinish, setIsFinish] = useState(false);
+    // const [isStart, setIsStart] = useState(false);
+
+    const [classNameForColor, setClassNameForColor] = useState(props.isFinish ? "box-finish" : props.isStart ? "box-start" : "");
+
+    // let classNameForColor = "";
+    // useEffect(() => {
+        
+    // },[]);
+
 
     
     return(
-        <div className="box">
+        <div className="box" style={{backgroundColor: props.color}}>
+    {console.log(props)}
         </div>
     );
 }
 
 export default Box;
+
+export const DEFAULT_NODE = {
+    row: 0,
+    col: 0,
+};
--- path-visualizer/src/PathfindingVisualizer.jsx
+++ path-visualizer/src/PathfindingVisualizer.jsx
@@ -10,10 +10,16 @@ function PathfindingVisualizer() {
 
     const getInitialGrid = () => {
         const grid = [];
-        for (let row = 0; row < 15; row++) {
+        for (let row = 0; row < 2; row++) {
           const currentRow = [];
-          for (let col = 0; col < 50; col++) {
-            currentRow.push([]);
+          for (let col = 0; col < 2; col++) {
+            let currentNode = {
+              col,
+              row,
+              isStart: row === 1 && col === 1,
+              isFinish: row === 2 && col === 2,
+            }
+            currentRow.push(currentNode);
           }
           grid.push(currentRow);
         }
@@ -23,17 +29,46 @@ function PathfindingVisualizer() {
 
     
     useEffect(() => {
-        setGrid(getInitialGrid());
-      });
+        const grid = [];
+        for (let row = 0; row < 10; row++) {
+          const currentRow = [];
+          for (let col = 0; col < 10; col++) {
+            let currentNode = {
+              col,
+              row,
+              isStart: row === 0 && col === 0,
+              isFinish: row === 9 && col === 9,
+            }
+            currentRow.push(currentNode);
+          }
+          grid.push(currentRow);
+        }
+        setGrid(grid);
+        // console.log(grid);
+      }, []);
 
     return (
         <div className="grid">
             {
                 grid.map((row, rowIdx) => {
+                  
                   return(
-                    row.map((node, nodeIdx) => <Box></Box>)
+                    <div key={rowIdx}>
+                    {row.map((box, boxIdx) => {
+                      const {isFinish, isStart} = box;
+                      const color = isFinish ? "red" : isStart ? "green" : "";
+                      return(
+                    <Box
+                      key={boxIdx}
+                      color={color}
+                      isStart={isStart}
+                      isFinish={isFinish}
+                    />
+                      );
+                })}
+                    </div>
                   );
-            })};
+            })}
         </div>
     );
 }
